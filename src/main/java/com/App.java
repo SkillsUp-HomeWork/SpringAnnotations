d6c146e983f7cc2c602a74b3458c3a7433f28c86
admin <xaker.dnepr@gmail.com>
2015-12-10 00:34:18 +0200

    done

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -13,6 +13,7 @@ public class App {
         System.out.println(contactsService.getAllContacts());
         contactsService.clearAll();
         System.out.println(contactsService.getAllContacts());
+        //q
 
     }
 }
