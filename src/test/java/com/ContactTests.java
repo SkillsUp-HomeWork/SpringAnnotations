e328fcca852a77efc0bf27b9dc07c1d348d789ea
admin <xaker.dnepr@gmail.com>
2015-12-13 11:20:56 +0200

    tests mockito

---
+++ src/test/java/com/ContactTests.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.mockito.Mockito.*;
+/**
+ * Created by admin on 13.12.2015.
+ */
+@RunWith(JUnit4.class)
+public class ContactTests {
+    @Test
+    public void testContactName(){
+        Contact mockContact = mock(Contact.class);
+        String testName = "name";
+        mockContact.setName(testName);
+        verify(mockContact, times(3)).setName(testName);
+
+    }
+}
