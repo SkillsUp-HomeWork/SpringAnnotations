9427471e3cfa22f717044a07c421ee515836f639
admin <xaker.dnepr@gmail.com>
2015-12-13 11:22:36 +0200

    tests mockito

--- src/test/java/com/ContactTests.java
+++ src/test/java/com/ContactTests.java
@@ -3,11 +3,8 @@ package com;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
 import static org.mockito.Mockito.*;
-/**
- * Created by admin on 13.12.2015.
- */
+
 @RunWith(JUnit4.class)
 public class ContactTests {
     @Test
