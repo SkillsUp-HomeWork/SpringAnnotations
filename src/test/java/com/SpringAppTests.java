6fc18c61074b411db2cdc5f3ba1053156e2da0e5
admin <xaker.dnepr@gmail.com>
2015-12-13 10:07:06 +0200

    tests

---
+++ src/test/java/com/SpringAppTests.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config.xml")
+public class SpringAppTests {
+    @Autowired
+    private ContactsService contactsService;
+
+    @Test
+    public void testStartContext(){
+    }
+}
